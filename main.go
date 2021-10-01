b166d15199d368102930f6fdb7c53fa716fe42d8
Ivan Sinyavsky <ivan-tg@mail.com>
2021-10-01 14:23:40 +0300

    тесты

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -27,25 +28,32 @@ func main() {
 
 	z = html.NewTokenizer(f)
 
-
+loop:
 	for {
 		tt := z.Next()
+		tgtext = standardizeSpaces(strings.TrimSpace(string(z.Text())))
+
 		switch tt {
 		case html.ErrorToken:
 			fmt.Println(z.Err())
 			return
 		case html.TextToken:
 			if depth > 0 {
-				tgtext = standardizeSpaces(strings.TrimSpace(string(z.Text())))
-				if tgtext != "" {
-					myLog.write(fmt.Sprintf("TEXT, %s, %s", tgname, tgtext))
-				}
+				//if tgtext != "" {
+				//	myLog.write(fmt.Sprintf("TEXT, %s, %s", tgname, tgtext))
+				//}
+				continue
 			}
 		case html.StartTagToken, html.EndTagToken:
 			if tt == html.StartTagToken {
 				tgname = tagname(z)
 				depth++
+				if tgname == `tbody` {
+					tableBodyParse()
+					break loop
+				}
 			} else {
+				//myLog.write(fmt.Sprintf("Close, %s, ", tagname(z)))
 				depth--
 				continue
 			}
@@ -55,6 +63,10 @@ func main() {
 			continue
 		}
 	}
+	fmt.Println(len(tableRows))
+	for i, j := range tableRows {
+		fmt.Println(i, j)
+	}
 }
 func tagname(t *html.Tokenizer) (out string) {
 	bs, _ := t.TagName()
@@ -62,6 +74,150 @@ func tagname(t *html.Tokenizer) (out string) {
 }
 
 func tableBodyParse() {
+	i:=0
+	inf := Information{}
+loop:
+	for {
+		tt := z.Next()
+		tgtext = standardizeSpaces(strings.TrimSpace(string(z.Text())))
+		switch tt {
+		case html.ErrorToken:
+			fmt.Println(z.Err())
+			return
+		case html.TextToken:
+			if depth > 0 {
+				if tgtext != "" {
+					fmt.Println("switch i", i)
+					switch i {
+					case 0:
+						inf.ProductName = tgtext
+						i++
+					case 1:
+						inf.Category = tgtext
+						i++
+					case 2:
+						tx := strings.Replace(tgtext, "(", "", -1)
+						tx = strings.Replace(tx, ")", "", -1)
+						nm, err := strconv.Atoi(tx)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 2: "))
+						}
+						inf.NumberOfReviews = int64(nm)
+						i++
+					case 3:
+						nm, err := strconv.Atoi(tgtext)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 3: "))
+						}
+						inf.SKU = int64(nm)
+						i++
+					case 4:
+						inf.Seller = tgtext
+						i++
+					case 5:
+						inf.Brand = tgtext
+						i++
+					case 6:
+						nm, err := strconv.Atoi(tgtext)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 6: "))
+						}
+						inf.QuantityInStock = int64(nm)
+						i++
+					case 7:
+						tx := strings.Replace(tgtext, ",", "", -1)
+						tx = strings.Replace(tx, ".", ",", -1)
+						tx = strings.Replace(tx, " руб.", "", -1)
+						tx = strings.Replace(tx, " руб,", "", -1)
+						nm, err := strconv.ParseFloat(tx,64)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 7: "))
+						}
+						inf.Price = nm
+						i++
+					case 8:
+						tx := strings.Replace(tgtext, "(", "", -1)
+						tx = strings.Replace(tx, ")", "", -1)
+						tx = strings.Replace(tx, "%", "", -1)
+						nm, err := strconv.Atoi(tx)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 8: "))
+						}
+						inf.Discount = int64(nm)
+						i++
+					case 9:
+						tx := strings.Replace(tgtext, ",", "", -1)
+						tx = strings.Replace(tx, ".", ",", -1)
+						tx = strings.Replace(tx, " руб.", "", -1)
+						tx = strings.Replace(tx, " руб,", "", -1)
+						nm, err := strconv.ParseFloat(tx,64)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 9: "))
+						}
+						inf.OldPrice = nm
+						i++
+					case 10:
+						tx := strings.Replace(tgtext, ",", "", -1)
+						tx = strings.Replace(tx, ".", ",", -1)
+						tx = strings.Replace(tx, " руб.", "", -1)
+						tx = strings.Replace(tx, " руб,", "", -1)
+						nm, err := strconv.ParseFloat(tx,64)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 10: "))
+						}
+						inf.ACP = nm
+						i++
+					case 11:
+						tx := strings.Replace(tgtext, ",", "", -1)
+						tx = strings.Replace(tx, ".", ",", -1)
+						tx = strings.Replace(tx, " руб.", "", -1)
+						tx = strings.Replace(tx, " руб,", "", -1)
+						nm, err := strconv.ParseFloat(tx,64)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 11: "))
+						}
+						inf.LP = nm
+						i++
+					case 12:
+						nm, err := strconv.Atoi(tgtext)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 12: "))
+						}
+						inf.AmountOfSales = int64(nm)
+						i++
+					case 13:
+						tx := strings.Replace(tgtext, ",", "", -1)
+						tx = strings.Replace(tx, ".", ",", -1)
+						tx = strings.Replace(tx, " руб.", "", -1)
+						tx = strings.Replace(tx, " руб,", "", -1)
+						nm, err := strconv.ParseFloat(tx,64)
+						if err != nil {
+							log.Fatalln(errors.Wrap(err, "case 13: "))
+						}
+						inf.Revenue = nm
+						i = 0
+						tableRows = append(tableRows, inf)
+						inf = Information{}
+					}
+				}
+			}
+		case html.StartTagToken, html.EndTagToken:
+			if tt == html.StartTagToken {
+				tgname = tagname(z)
+				depth++
+			} else {
+				depth--
+				if tagname(z) == `tbody` {
+					break loop
+				}
+				continue
+			}
+		case html.SelfClosingTagToken:
+			continue
+		default:
+			continue
+		}
+	}
 
 }
 
@@ -74,7 +230,7 @@ type Information struct {
 	Brand           string
 	QuantityInStock int64
 	Price           float64
-	Discount        string
+	Discount        int64
 	OldPrice        float64
 	ACP             float64
 	LP              float64
